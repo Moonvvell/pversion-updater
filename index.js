4c793dfdc3e86b165bb4467fefc13fc09a01dd9d
moonvvell <moonvvell@ukr.net>
2022-06-08 19:16:57 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const { exit, exec } = require('shelljs');
-const fs = require('fs');
-const packageFile = require('./package.json');
 
 class VersionUpdater {
   constructor(mainBranch = 'main', releaseBranch = 'release') {
     this.mainBranch = mainBranch;
     this.releaseBranch = releaseBranch;
     [this.versionUpdateType] = process.argv.slice(2);
-    this.runExecCommand = this.runExecCommand.bind(this);
   }
 
   update() {
     this.ensureRunOnlyOnMasterBranch();
-    this.updateGitRemotes();
-    this.updatePackageJsonVersionCode();
+    VersionUpdater.updateGitRemotes();
     this.updatePackageJsonVersion();
     this.updateCodeRepository();
     exit(0);
   }
 
   ensureRunOnlyOnMasterBranch() {
-    const runBranch = this.runExecCommand('git rev-parse --abbrev-ref HEAD');
+    const runBranch = VersionUpdater.runExecCommand('git rev-parse --abbrev-ref HEAD');
     if (runBranch.trim() !== this.mainBranch.trim()) {
       console.info(`You tried to run script on ${runBranch} branch. It should be run only on ${this.mainBranch}`);
       exit(0);
     }
   }
 
-  runExecCommand(command) {
+  static runExecCommand(command) {
     console.info(`Running command: ${command}`);
     const execCommand = exec(command, { silent: true });
     if (execCommand.code !== 0) {
@@ -41,38 +37,27 @@ class VersionUpdater {
      * Because most of CI\CD store remote only for branch you run it,
      * we need to update information about all branches
      */
-  updateGitRemotes() {
-    this.runExecCommand('git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"');
-    this.runExecCommand('git fetch');
+  static updateGitRemotes() {
+    VersionUpdater.runExecCommand('git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"');
+    VersionUpdater.runExecCommand('git fetch');
   }
 
   updatePackageJsonVersion() {
     const { versionUpdateType } = this;
     if (versionUpdateType === 'minor') {
-      this.runExecCommand('yarn version --minor');
+      VersionUpdater.runExecCommand('yarn version --minor');
     } else if (versionUpdateType === 'major') {
-      this.runExecCommand('yarn version --major');
+      VersionUpdater.runExecCommand('yarn version --major');
     } else {
-      this.runExecCommand('yarn version --patch');
+      VersionUpdater.runExecCommand('yarn version --patch');
     }
   }
 
   updateCodeRepository() {
-    this.runExecCommand('git push');
-    this.runExecCommand('git fetch');
-    this.runExecCommand(`git checkout ${this.releaseBranch}`);
-    this.runExecCommand(`git merge ${this.mainBranch} --allow-unrelated-histories`);
-    this.runExecCommand('git push');
-  }
-
-  updatePackageJsonVersionCode() {
-    packageFile.versionCode += 1;
-    try {
-      fs.writeFileSync('./package.json', JSON.stringify(packageFile), 'utf-8');
-    } catch (err) {
-      console.error(err);
-      this.runExecCommand('git reset --hard');
-      exit(1);
-    }
+    VersionUpdater.runExecCommand('git push');
+    VersionUpdater.runExecCommand('git fetch');
+    VersionUpdater.runExecCommand(`git checkout ${this.releaseBranch}`);
+    VersionUpdater.runExecCommand(`git merge ${this.mainBranch} --allow-unrelated-histories`);
+    VersionUpdater.runExecCommand('git push');
   }
 }
